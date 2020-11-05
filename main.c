74075d3bb443d51c9409f68ea072e12be605c448
KarlWithK <jocelinc60@outlook.com>
2020-11-05 15:16:03 -0500

    Just testing somethings out

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 int main() {
 	struct song *shux[27];
+	for(int i = 0; i < 27; i++) {
+		shux[i] = NULL;
+	}
 	shux[0] = new_song("ATM", "JCole");
 	print_list(shux[0]);
 	struct song *free = free_list(shux[0]);
