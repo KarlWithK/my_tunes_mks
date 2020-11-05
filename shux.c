eeae387fdc31202b62fc3ffbc4ccd39b503b0b52
Jacky <jchen10@stuy.edu>
2020-11-05 11:00:32 -0500

    added a variety of functions

--- shux.c
+++ shux.c
@@ -1,1 +1,60 @@
 #include "shux.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "construction.h"
+#include <time.h>
+
+struct song *new_song(char *n, char *a){
+  struct song *np = malloc(sizeof(struct song));
+
+  np->name = strdup(n);
+  np->artist = strdup(a);
+  np->next = NULL;
+  return np;
+}
+
+struct song *insert_front(struct song *s, char *a, char *n){
+    struct song *np = new_song(n,a);
+    np->next = s;
+    return np;
+}  
+
+struct song *free_song(struct song *s){
+  free(s->artist);
+  free(s->name);
+  free(s);
+  return NULL;
+}
+
+struct song *find_song(struct song *s, char *a,char *n){
+    while(s){
+        if (!strcmp(s->artist,a) && !strcmp(s->name,n)) return s;
+        s = s->next;
+    }
+    return NULL;
+}
+
+struct song *find_firstS(struct song*s, char *a){
+    while(s){
+        if (!strcmp(s->artist,a)) return s;
+        s = s->next;
+    }
+    return NULL;
+}
+
+
+void print_list(struct song *s){
+  printf("[");
+  while (s) {
+    printf("%s, ", s->name);
+    s = s->next;
+  }
+  printf("]\n");
+}
+
+
+struct song *random_find(struct song *s){
+    srand(time(NULL));
+    int i = rand()%100;
+}
--- shux.h
+++ shux.h
@@ -5,4 +5,20 @@ struct song {
   char *artist;
   struct song *next;
 };
+
+struct song *new_song(char *n, char *a);
+
+struct song *insert_front(struct song *s, char *a,char *n );
+
+struct song *free_song(struct song *s);
+
+struct song *find_song(struct song *s, char *a,char *n);
+
+//finds the first song of an artist
+struct song *find_firstS(struct song*s, char *a);
+
+void print_list(struct song *s);
+
+struct song *random_find(struct song *s);
+
 #endif /* ifndef SHUX_LIST */
