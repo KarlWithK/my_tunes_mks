b7c40d8d4252787a1f01c842d48064ca6a723988
KarlWithK <jocelinc60@outlook.com>
2020-11-12 09:07:38 -0500

    Ahhhhhhhh random is hard

--- main.c
+++ main.c
@@ -5,20 +5,48 @@
 int main() {
   char linebreak[] = "====================================";
   struct library *lib = new_lib();
-  struct song *test = new_song("Whip", "JCole");
-  lib = lib_add_song(lib, test);
-  struct song *hello = new_song("wrp", "JCole");
-  lib = lib_add_song(lib, hello);
-  struct song *bruh= new_song("Waa", "JCole");
-  lib = lib_add_song(lib,bruh );
-  lib_print_by_letter(lib, 'j');
-  lib = lib_delete_song(lib, bruh);
-  /* print_list(lib->position[9]); */
-
-  lib_print_by_letter(lib, 'j');
+  printf("%s\n", "LINKED LIST TESTS");
+  printf("%s\n", linebreak);
+  struct song *test1 = new_song("ATM", "JCole");
+  struct song *test2 = new_song("Heros", "David Bowie");
+  struct song *test3 = new_song("Edge Cases", "%Case");
+  struct song *test4 = new_song("Gernade", "Bruno Mars");
+  struct song *test5 = new_song("Chunky", "Bruno Mars");
+  lib = lib_add_song(lib, test1);
+  lib = lib_add_song(lib, test2);
+  lib = lib_add_song(lib, test3);
+  lib = lib_add_song(lib, test4);
+  lib = lib_add_song(lib, test5);
+  printf("%s\n", "Testing print_list");
+  print_list(lib->position[3]);
+  /* lib_print_all(lib); */
+  printf("%s\n", linebreak);
+  printf("%s\n", "Testing print_node");
+  print_song(test3);
+  printf("\n%s\n", linebreak);
+  printf("%s\n", "Testing find_node");
+  printf("%s [%s : %s]\n", "looking for", test1->artist, test1->name);
+  printf("%s %s : %s\n", "Node found!", test1->artist, test1->name);
+  print_song(lib_find_song(lib, test1->name, test1->artist));
+  printf("%s [%s : %s]\n", "looking for", test1->artist, "Photograph");
+  printf("%s\n", "Node not found!");
+  print_song(lib_find_song(lib, "Photograph", test1->artist));
+  printf("\n%s\n", linebreak);
+  printf("%s [%s]\n", "looking for", "Bruno Mars");
+  lib_print_by_artist(lib, "Bruno Mars");
+  printf("\n%s [%s]\n", "looking for", "BTS");
+  lib_print_by_artist(lib, "BTS");
+  printf("%s\n", linebreak);
+  printf("%s\n", "Testing random");
+  /* lib_print_random(lib); */
+  printf("%s\n", linebreak);
+  printf("%s\n", "print_libary");
+  lib_print_all(lib);
+  printf("%s\n", linebreak);
+  printf("%s\n", "Testing print_letter");
+  lib_print_by_letter(lib, 'b');
   lib_clear_all(lib);
   lib_free(lib);
-  print_list(lib->position[9]);
 
   /* lib_print_random(lib); */
   /* lib_print_by_letter(lib, 'j'); */
@@ -44,17 +72,6 @@ int main() {
 
 /* lib_print_all(lib1);*/
 
-/* printf("\n\nRandom Find Test\n\n");*/
-/* * random_find test*/
-/* struct song* s = new_song("h","A");*/
-/* struct song* k = new_song("h","B");*/
-/* struct song* h = new_song("h","C");*/
-/* struct song* z = new_song("h","D");*/
-/* s->next = k;*/
-/* k->next = h;*/
-/* h->next = z;*/
-
-/* printf("%s\n", random_find(s)->artist);*/
 /*
 
 
