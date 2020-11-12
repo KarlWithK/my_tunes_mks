2ff0d47c3de7fe345697d76340762077a9d5e055
KarlWithK <jocelinc60@outlook.com>
2020-11-12 05:40:09 -0500

    made some changes

--- main.c
+++ main.c
@@ -2,34 +2,39 @@
 #include "shux.h"
 
 int main() {
-	char a = 'A';
-	printf("char: %c and num: %d\n",a,a);
-	struct library * lib1= new_lib();
-	lib1->position[0]=new_song("ATM", "JCole");
-	lib1->position[1]=new_song("Bob", "bobby");
-
-	lib_print_all(lib1);
+		char linebreak[] = "====================================";
 
 
-
-
-
-
-
-	printf("\n\nRandom Find Test\n\n");
-	///* random_find test
-	struct song* s = new_song("h","A");
-	struct song* k = new_song("h","B");
-	struct song* h = new_song("h","C");
-	struct song* z = new_song("h","D");
-	s->next = k;
-	k->next = h;
-	h->next = z;
-
-	printf("%s\n", random_find(s)->artist);
-	//*/
-
-
-
-	return 0;
 }
+
+	/* char a = 'A';*/
+	/* printf("char: %c and num: %d\n",a,a);*/
+	/* struct library * lib1= new_lib();*/
+	/* lib1->position[0]=new_song("ATM", "JCole");*/
+	/* lib1->position[1]=new_song("Bob", "bobby");*/
+
+	/* lib_print_all(lib1);*/
+
+
+
+
+
+
+
+	/* printf("\n\nRandom Find Test\n\n");*/
+	/* ///* random_find test*/
+	/* struct song* s = new_song("h","A");*/
+	/* struct song* k = new_song("h","B");*/
+	/* struct song* h = new_song("h","C");*/
+	/* struct song* z = new_song("h","D");*/
+	/* s->next = k;*/
+	/* k->next = h;*/
+	/* h->next = z;*/
+
+	/* printf("%s\n", random_find(s)->artist);*/
+	/* //*/*/
+
+
+
+	/* return 0;*/
+/* } */
